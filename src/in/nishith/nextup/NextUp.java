797399fb45346b4af0f65d84e767f523a7203b16
Nishith Nand <nishith.nand@gmail.com>
2011-05-12 23:00:53 +0530

    Added the complete Android project.

---
+++ src/in/nishith/nextup/NextUp.java
@@ -0,0 +1,13 @@
+package in.nishith.nextup;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NextUp extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
